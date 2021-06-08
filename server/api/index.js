49e464a71e74ee8c0164a1d9802c3aa56ee98bb6
j-dags <jon.dagdagan@gmail.com>
2021-06-08 00:38:02 -0500

    removed python files

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/processing', require('./processing'))
-router.use('/python', require('./python'))
+// router.use('/python', require('./python'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
